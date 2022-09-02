615a36fe5dfd40c7dae46131da7781ce17dc6d94
leeyuna22 <109680562+leeyuna22@users.noreply.github.com>
2022-09-02 23:16:48 +0900

    test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
 import Layout from "../components/Layout";
 
 const Main = () => {
   return (
-      <>
-      <Header/>
-      <Layout>
-      </Layout>
-      </>
-      
-     
-    
+    <>
+      <Header />
+      <Layout></Layout>
+      <p>ddd</p>
+    </>
   );
-}
+};
 
-export default Main
+export default Main;
