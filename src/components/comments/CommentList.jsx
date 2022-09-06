0db813ecc8a7ae958e19f100e867cd2080d20f16
leeyuna22 <lyn120303@naver.com>
2022-09-06 14:43:12 +0900

    상세 페이지 댓글 리스트 구현중

--- src/components/comments/CommentList.jsx
+++ src/components/comments/CommentList.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
-import Comment from './Comment'
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
 
 const CommentList = () => {
   return (
     <div>
-        <Comment/>
-          {/* {commentList.map((comment) => {
-            return <Comment key={comment._id} commentObj={comment} />;
-          })} */}
-
+      {commentList.map((comment) => {
+        return <Comment key={comment.id} commentObj={comment} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default CommentList
+export default CommentList;
